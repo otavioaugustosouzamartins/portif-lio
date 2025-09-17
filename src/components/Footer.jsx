4f1814415c7f6c92b6cdecb9c4b0a1171a3e36e5
Otávio Augusto <otaviomartins2002@gmail.com>
2025-09-17 16:24:55 -0300

    Fix footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,8 @@
 export default function Footer(){
     return(
         <footer className="footer">
-            <div className="container" style={{display: "flex",justifyContent:"space-between",alignItems:"center"}}>
+            <div className="container-box" >
              <span>© {new Date().getFullYear()} Otávio.</span>
-             <a href="#home" className="subtle" style={{textDecoration:"none"}}>Voltar ao topo ↑</a>
              </div>
         </footer>
     )
